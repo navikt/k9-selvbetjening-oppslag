2999c43b761f1780fb2ea903bbae885c6fc32def
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-09-02 14:28:57 +0200

    Struktur på prosjektet.

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+val dusseldorfKtorVersion = "1.2.3.b45ba92"
+val mainClass = "no.nav.k9.selvbetjening.oppslag.K9SelvbetjeningOppslagKt"
+
+
+plugins {
+    kotlin("jvm") version "1.3.41"
+    id("com.github.johnrengelman.shadow") version "5.1.0"
+}
+
+buildscript {
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/b45ba92fbe655e1818374daf6465af5174a72183/gradle/dusseldorf-ktor.gradle.kts")
+}
+
+dependencies {
+    compile ( "no.nav.helse:dusseldorf-ktor-core:$dusseldorfKtorVersion")
+}
+
+repositories {
+    maven("https://dl.bintray.com/kotlin/ktor")
+    maven("https://kotlin.bintray.com/kotlinx")
+    maven("http://packages.confluent.io/maven/")
+
+    jcenter()
+    mavenLocal()
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<ShadowJar> {
+    archiveBaseName.set("app")
+    archiveClassifier.set("")
+    manifest {
+        attributes(
+            mapOf(
+                "Main-Class" to mainClass
+            )
+        )
+    }
+}
+
+tasks.withType<Wrapper> {
+    gradleVersion = "5.6.1"
+}
