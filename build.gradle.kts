2b38e2cadecac024ba180f4ac953a5030ef84cd4
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-09-02 14:30:59 +0200

    Dependencies oppdatering

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-val dusseldorfKtorVersion = "1.2.3.b45ba92"
+val dusseldorfKtorVersion = "1.2.3.664b246"
 val mainClass = "no.nav.k9.selvbetjening.oppslag.K9SelvbetjeningOppslagKt"
 
 
 plugins {
-    kotlin("jvm") version "1.3.41"
+    kotlin("jvm") version "1.3.50"
     id("com.github.johnrengelman.shadow") version "5.1.0"
 }
 
 buildscript {
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/b45ba92fbe655e1818374daf6465af5174a72183/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/664b2468f6d1a30d77dca88a57eb240e26c32087/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
