fd16dcbc1f6934f1722c6847dd3a3dfdcece6ff0
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-04-17 17:24:56 +0200

    version update dusseldorf-ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-val dusseldorfKtorVersion = "1.3.2.fd126a5"
+val dusseldorfKtorVersion = "1.3.2.dcec86c"
 val ktorVersion = ext.get("ktorVersion").toString()
 val kotlinVersion = ext.get("kotlinVersion").toString()
 
@@ -15,7 +15,7 @@ plugins {
 }
 
 buildscript {
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/fd126a5dd32e8df5c6c6a3dd7e4b77988846be6e/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/dcec86c3a9caa91f16cb600dd4be66ade7e70797/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
