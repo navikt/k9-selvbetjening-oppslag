64b6a4afb17573ece27d489c41d683a603487ec8
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-06-29 09:41:23 +0200

    Oppdaterer avhengigheter. Exclyder kotlin-test-junit fra ktor-server-test-host fordi det kræsjer med Gradle som også trekker det inn. Ref https://youtrack.jetbrains.com/issue/KT-46090 og https://github.com/gradle/gradle/issues/17137

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,24 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-val dusseldorfKtorVersion = "1.5.2.fa18872"
+val dusseldorfKtorVersion = "2.1.6.0-ef0acb6"
 val ktorVersion = ext.get("ktorVersion").toString()
 val kotlinVersion = ext.get("kotlinVersion").toString()
 
-val mockkVersion = "1.10.5"
+val mockkVersion = "1.11.0"
 val jsonassertVersion = "1.5.0"
-val junitJupiterVersion = "5.7.0"
+val junitJupiterVersion = "5.7.2"
+val fuelVersion = "2.3.1"
 
 val mainClass = "no.nav.k9.SelvbetjeningOppslagKt"
 
 plugins {
-    kotlin("jvm") version "1.4.21"
-    id("com.github.johnrengelman.shadow") version "6.0.0"
+    kotlin("jvm") version "1.5.20"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 buildscript {
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/fa1887241239fdd1877f0f258a94872281db8783/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/ef0acb6425e85073932e8d021e33849110f58159/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
@@ -26,11 +27,16 @@ dependencies {
     implementation ( "no.nav.helse:dusseldorf-ktor-client:$dusseldorfKtorVersion")
     implementation ( "no.nav.helse:dusseldorf-ktor-metrics:$dusseldorfKtorVersion")
     implementation ( "no.nav.helse:dusseldorf-oauth2-client:$dusseldorfKtorVersion")
+    implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
+    implementation("com.github.kittinunf.fuel:fuel-coroutines:$fuelVersion"){
+        exclude(group = "org.jetbrains.kotlinx", module = "kotlinx-coroutines-core")
+    }
 
     // Test
     testImplementation ( "no.nav.helse:dusseldorf-test-support:$dusseldorfKtorVersion")
     testImplementation ("io.ktor:ktor-server-test-host:$ktorVersion") {
         exclude(group = "org.eclipse.jetty")
+        exclude("org.jetbrains.kotlin", "kotlin-test-junit") // https://github.com/gradle/gradle/issues/17137
     }
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
@@ -52,11 +58,10 @@ repositories {
     }
 
     mavenCentral()
-    jcenter()
-
+    maven("https://jitpack.io")
     maven("https://dl.bintray.com/kotlin/ktor")
     maven("https://kotlin.bintray.com/kotlinx")
-    maven("http://packages.confluent.io/maven/")
+    maven("https://packages.confluent.io/maven/")
 }
 
 java {
@@ -106,5 +111,5 @@ tasks.register<ShadowJar>("shadowJarWithMocks") {
 }
 
 tasks.withType<Wrapper> {
-    gradleVersion = "6.7.1"
+    gradleVersion = "7.1"
 }
