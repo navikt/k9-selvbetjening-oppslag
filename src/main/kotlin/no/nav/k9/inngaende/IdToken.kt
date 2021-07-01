cb737a150b14a616882292f236beb4aa7f9645f0
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-07-01 09:21:31 +0200

    Legger til støtte for å hente pid fra innkommende token.

--- src/main/kotlin/no/nav/k9/inngaende/IdToken.kt
+++ src/main/kotlin/no/nav/k9/inngaende/IdToken.kt
@@ -1,6 +1,7 @@
 package no.nav.k9.inngaende
 
 import com.auth0.jwt.JWT
+import com.auth0.jwt.interfaces.DecodedJWT
 import io.ktor.application.ApplicationCall
 import io.ktor.auth.parseAuthorizationHeader
 import no.nav.k9.inngaende.oppslag.Ident
@@ -12,10 +13,17 @@ internal data class IdToken(
     )
 )
 
-fun subject(value: String): String =
-    JWT.decode(value).subject ?: JWT.decode(value).getClaim("pid").asString() ?: throw IllegalStateException("Token mangler 'sub'/'pid' claim.")
+fun subject(value: String): String {
+    val token: DecodedJWT = JWT.decode(value)
+    return when {
+        token.issuer.contains("tokendings") -> token.getClaim("pid").asString()
+            ?: throw IllegalStateException("Token mangler 'pid' claim.")
 
-internal fun ApplicationCall.idToken() : IdToken {
+        else -> token.subject ?: throw IllegalStateException("Token mangler 'sub' claim.")
+    }
+}
+
+internal fun ApplicationCall.idToken(): IdToken {
     val jwt = request.parseAuthorizationHeader()?.render() ?: throw IllegalStateException("Token ikke satt")
     return IdToken(jwt.substringAfter("Bearer "))
 }
