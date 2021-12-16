0aedd26f0fa8c1bc7f8076d58a1a14cb3d4cbb11
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-12-16 12:34:53 +0100

    Oppdaterer dusseldorf-ktor, plugins og pakker.
    - Tillater dependabot å lage PR for dusseldorf-ktor og sif-tilgangskontroll oppdateringer.
    - closes #90
    - closes #89
    - closes #88
    - closes #85
    - closes #83
    - closes #82

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,25 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-val dusseldorfKtorVersion = "3.1.6.4-e07c5ec"
+val dusseldorfKtorVersion = "3.1.6.7-7d29e37"
 val ktorVersion = ext.get("ktorVersion").toString()
 val kotlinVersion = ext.get("kotlinVersion").toString()
-val graphqlKotlinClientVersion = "5.2.0"
+val graphqlKotlinClientVersion = "5.3.1"
 val sifTilgangskontrollVersion = "1-58dcba8"
 
-val mockkVersion = "1.12.0"
+val mockkVersion = "1.12.1"
 val jsonassertVersion = "1.5.0"
 val fuelVersion = "2.3.1"
 
 val mainClass = "no.nav.k9.SelvbetjeningOppslagKt"
 
 plugins {
-    kotlin("jvm") version "1.5.31"
-    id("com.github.johnrengelman.shadow") version "7.1.0"
+    kotlin("jvm") version "1.6.10"
+    id("com.github.johnrengelman.shadow") version "7.1.1"
 }
 
 buildscript {
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/e07c5ecf831928eb250c946e753aff2a3b798295/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/109ae9ba66daf97083e807d314a515fd72d6e69c/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
