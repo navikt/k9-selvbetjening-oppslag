2add6801ad91ef4ad4f5aa904401302c0db23224
Sander Opperud Odden <sander.opperud.odden@nav.no>
2022-08-16 10:49:11 +0200

    Legger til mer logging ved diff for å snevre inn mulig problemområde.

--- src/main/kotlin/no/nav/k9/inngaende/oppslag/OppslagService.kt
+++ src/main/kotlin/no/nav/k9/inngaende/oppslag/OppslagService.kt
@@ -58,6 +58,11 @@ internal class OppslagService(
         arbeidsgivere?.also {
             val arbeidsgivereFraV1erLikV2 = arbeidsgivere == arbeidsgivereFraV2
             logger.info("Migreringsjekk til aareg v2. Er like=$arbeidsgivereFraV1erLikV2")
+            if(!arbeidsgivereFraV1erLikV2){
+                logger.info("Migreringsjekk til aareg v2. Antall organisasjoner: V1=${arbeidsgivere.organisasjoner.size}, V2=${arbeidsgivereFraV2?.organisasjoner?.size}")
+                logger.info("Migreringsjekk til aareg v2. Antall frilansoppdrag: V1=${arbeidsgivere.frilansoppdrag.size}, V2=${arbeidsgivereFraV2?.frilansoppdrag?.size}")
+                logger.info("Migreringsjekk til aareg v2. Antall private arbeidsgivere: V1=${arbeidsgivere.privateArbeidsgivere.size}, V2=${arbeidsgivereFraV2?.privateArbeidsgivere?.size}")
+            }
         }
 
         val meg = megOppslag.meg(
