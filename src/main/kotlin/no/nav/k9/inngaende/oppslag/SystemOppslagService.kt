d30eda76d40bafa96d25353d2d392fa2d9354bd4
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-03-06 13:55:16 +0100

    Logger henting av identer.

--- src/main/kotlin/no/nav/k9/inngaende/oppslag/SystemOppslagService.kt
+++ src/main/kotlin/no/nav/k9/inngaende/oppslag/SystemOppslagService.kt
@@ -3,12 +3,18 @@ package no.nav.k9.inngaende.oppslag
 import no.nav.k9.utgaende.gateway.PDLProxyGateway
 import no.nav.siftilgangskontroll.pdl.generated.enums.IdentGruppe
 import no.nav.siftilgangskontroll.pdl.generated.hentidenterbolk.HentIdenterBolkResult
+import org.slf4j.LoggerFactory
 
 class SystemOppslagService(
     private val pdlProxyGateway: PDLProxyGateway
 ) {
 
+    private companion object {
+        private val logger = LoggerFactory.getLogger(SystemOppslagService::class.java)
+    }
+
     suspend fun hentIdenter(identer: List<String>, identGrupper: List<IdentGruppe>): List<HentIdenterBolkResult> {
+        logger.info("Henter identer med systemkall.")
         return pdlProxyGateway.hentIdenter(identer, identGrupper)
     }
 }
