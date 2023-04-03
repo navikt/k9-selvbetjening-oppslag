1f3809b7cd4d6b7b7b4b3afb88eee5a1c20e03a6
Ramin Esfandiari <ramin.esfandiari@nav.no>
2023-04-03 11:43:16 +0200

    Oppdaterer gradle wrapper, pakker, og plugins. (#224)
    * Oppdaterer gradle wrapper, pakker, og plugins.
    
    Erstatter deprektert kode i RestUtils.
    
    - closes #223
    - closes #222
    - closes #221
    - closes #220
    - closes #219
    - closes #218
    - closes #217
    - closes #216
    - closes #215
    
    * Debugger SSL handshake
    
    * Legger til manglende externalHosts konfigurasjon.
    
    * Fikser feil i mapping av handlebars for externalHosts.

--- src/main/kotlin/no/nav/k9/utgaende/rest/RestUtils.kt
+++ src/main/kotlin/no/nav/k9/utgaende/rest/RestUtils.kt
@@ -1,13 +1,13 @@
 package no.nav.k9.utgaende.rest
 
+import net.minidev.json.parser.JSONParser
 import no.nav.helse.dusseldorf.ktor.core.templateQueryParameters
 import org.json.JSONArray
 import org.json.JSONObject
-import org.json.simple.parser.JSONParser
 import org.slf4j.Logger
 
 private object RestUtils {
-    internal val parser = JSONParser()
+    internal val parser = JSONParser(JSONParser.DEFAULT_PERMISSIVE_MODE)
 }
 
 internal fun Logger.restKall(url: String, urlTemplate: Boolean = false) = info("Utgående kall til ${if (urlTemplate) url.templateQueryParameters() else url}")
@@ -20,4 +20,4 @@ internal fun JSONObject.getStringOrNull(key: String) = if (has(key) && !isNull(k
  * ArbeidsgiverOgArbeidstakerRegisterV1 returnerer ugyldig json som gir org.json.JSONException: Duplicate key
  * org.json.simple håndterer dette med å ta sisste key istedenfor å feile. Wrapper håndteringen for å unngå denne feilen.
  */
-internal fun String.somJsonArray() = JSONArray((RestUtils.parser.parse(this) as org.json.simple.JSONArray).toJSONString())
+internal fun String.somJsonArray() = JSONArray((RestUtils.parser.parse(this) as net.minidev.json.JSONArray).toJSONString())
