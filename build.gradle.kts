aab793af29691089e35368a0a3e81d47ac785e1c
Ramin Esfandiari <ramin.esfandiari@nav.no>
2023-08-15 06:48:21 +0000

    Fikser sifTilgangskontrollVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 val dusseldorfKtorVersion = "4.0.4"
 val ktorVersion = "2.3.3"
 val graphqlKotlinClientVersion = "6.5.3"
-val sifTilgangskontrollVersion = "1-6079482"
+val sifTilgangskontrollVersion = "1.0.1"
 val tokenSupportVersion = "3.1.3"
 val mockOauth2ServerVersion = "1.0.0"
 
