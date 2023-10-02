b6aea1ce1db9e03cb3973cfae32a83187ca5eb8c
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-10-02 11:15:53 +0200

    Bumper opp sifTilgangskontrollVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 val dusseldorfKtorVersion = "4.0.10"
 val ktorVersion = "2.3.4"
 val graphqlKotlinClientVersion = "7.0.1"
-val sifTilgangskontrollVersion = "1.0.1"
+val sifTilgangskontrollVersion = "2.0.1"
 val tokenSupportVersion = "3.1.5"
 val mockOauth2ServerVersion = "2.0.1"
 
