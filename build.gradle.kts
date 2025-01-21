47621dce021a3dc5bd67097753aeafde6822814f
Helga.Nyrud <helga.nyrud@nav.no>
2025-01-21 09:51:01 +0100

    Nedgraderer kotlin ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 val dusseldorfKtorVersion = "5.0.17"
 val ktorVersion = "2.3.13"
-val graphqlKotlinClientVersion = "8.3.0"
+val graphqlKotlinClientVersion = "8.2.1"
 val sifTilgangskontrollVersion = "5.1.0"
 val tokenSupportVersion = "5.0.14"
 val mockOauth2ServerVersion = "2.1.10"
