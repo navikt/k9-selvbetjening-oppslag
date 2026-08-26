57f1fbe39f771b720237b2550b3b5d334be6f2a1
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2026-08-26 09:12:06 +0200

    Downgrade graphqlKotlinClientVersion pga jackson 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val dusseldorfKtorVersion = "8.0.2"
 val ktorVersion = "3.1.0"
-val graphqlKotlinClientVersion = "10.2.1"
+val graphqlKotlinClientVersion = "9.2.1"
 val sifTilgangskontrollVersion = "5.3.2"
 val tokenSupportVersion = "6.0.11"
 val mockOauth2ServerVersion = "6.0.2"
